3e3cf2a2cc319c1b69e27907be837103771c753b
leva.kalashnikov@gmail.com <leva.kalashnikov@gmail.com>
2025-04-10 23:11:33 +0300

    works

--- cluster-tester.py
+++ cluster-tester.py
@@ -33,8 +33,19 @@ def extract_segments(array, mask):
 
     return segments
 
+def cut_end_zeros(array):
+    plot_series = array
+    for _ in range(len(plot_series) - 1, 0, -1):
+        if plot_series[_] != 0:
+            return plot_series
+        else:
+            plot_series = np.delete(plot_series, _)
+
+
 segments = {}
-for i in range(5):
+nums = list(range(10))+[29,31,33,41,48,53,114,155,54,153,73]
+
+for i in nums:
     extract_segments(signals[i, 1, :], masks[i, 1, :])
 
 # Теперь кластеризация
@@ -52,7 +63,8 @@ for i in range(len(series)):
 
 series=np.array(series)
 
-'''
+# Кластеризация через KMeans
+
 model = KMeans(k=3, max_it=15, max_dba_it=15, dists_options={"window": 40})
 cluster_idx, performed_it = model.fit(series, use_parallel=False)
 centers  = model.kmeansplusplus_centers(series=series)
@@ -64,28 +76,32 @@ for i in range(len(cluster_idx)):
 print(cluster_idx, performed_it)
 
 for i in range(len(cluster_idx)):
-    for j in range(len(cluster_idx[i])):
-        axs[i, 0].plot(series[j])
-    axs[i, 1].plot(centers[i])
+    for j in range(0, len(cluster_idx[i]), 10):
+        axs[i, 0].plot(cut_end_zeros(series[j]))
+    axs[i, 1].plot(cut_end_zeros(centers[i]))
 
 plt.show()
-'''
+
 
 
 # Собираем всех по dtw
-s1 = series[38]
-s2 = series[0]
-s3 = series[10]
+potential_centroids  = [0, 101, -15]
 
-plt.plot(s1, c='b')
-plt.plot(s2, c='orange')
-plt.plot(s3, c='g')
+s1 = series[potential_centroids[0]]
+s2 = series[potential_centroids[1]]
+s3 = series[potential_centroids[2]]
+
+
+plt.plot(cut_end_zeros(s1), c='b')
+plt.plot(cut_end_zeros(s2), c='orange')
+plt.plot(cut_end_zeros(s3), c='g')
 plt.show()
 
-clusters = {0:[series[38]], 1:[series[0]], 2:[series[10]]}
+
+clusters = {0:[s1], 1:[s2], 2:[s3]}
 
 for i in range(len(series)):
-    if i not in (0, 10, 38):
+    if i not in (potential_centroids):
         distances = [dtw.distance(series[i], clusters[0][0]),
                      dtw.distance(series[i], clusters[1][0]),
                      dtw.distance(series[i], clusters[2][0])]
@@ -94,8 +110,9 @@ for i in range(len(series)):
 fig, axs = plt.subplots(len(clusters), 2)
 
 for i in range(len(clusters)):
-    for j in range(len(clusters[i])):
-        axs[i, 0].plot(series[j])
-    axs[i, 1].plot(clusters[i][0])
+    for j in range(0, len(clusters[i]), 15):
+        plot_series = cut_end_zeros(series[j])
+        axs[i, 0].plot(plot_series)
+    axs[i, 1].plot(cut_end_zeros(clusters[i][0]))
 
 plt.show()
